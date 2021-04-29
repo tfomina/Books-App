e50867738289d4077954b24d54e7951fef21da19
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-29 21:40:23 +0300

    Установила пакеты bcrypt, passport, passport-local, обновила mongoose, добавила модель user, несколько роутов для юзера и шаблоны для юзера

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,55 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
+const { User } = require("../models");
+
+// страница с формой регистрации
+router.get("/register", (req, res) => {
+  res.render("user/register", {
+    title: "Регистрация",
+    btnTitle: "Зарегистрироваться",
+    user: {},
+  });
+});
+
+// регистрация пользователя
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  let candidate;
+  try {
+    candidate = await User.findOne({ email });
+
+    if (candidate) {
+      res.send(`Email ${email} уже занят`);
+    } else {
+      const passwordHash = await bcrypt.hash(password, 10);
+      const user = new User({
+        email,
+        name,
+        passwordHash,
+      });
+
+      await user.save();
+      res.redirect("/");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// страница с формой входа
+router.get("/login", (req, res) => {
+  res.render("user/login", { title: "Вход", btnTitle: "Войти", user: {} });
+});
 
 // авторизация пользователя
-router.post("/login", (req, res) => {
-  res.status(201).json({ id: 1, mail: "test@mail.ru" });
+router.post("/login", (req, res) => {});
+
+router.get("/profile", (req, res) => {
+  res.render("user/profile", {
+    title: "Профиль пользователя",
+  });
 });
 
 module.exports = router;
